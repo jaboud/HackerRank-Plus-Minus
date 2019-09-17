9dbda4c0c63f701c4c39a3a29ecb56f741539da4
jaboud <jaboud94@gmail.com>
2019-09-17 15:06:05 +1000

    Solution creation and declared a static method using integers.

---
+++ HackerRank-Plus-Minus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HackerRank_Plus_Minus
+{
+    class Program
+    {
+        static void PlusMinus(int[] arr)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
